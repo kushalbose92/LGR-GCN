09fc97d9592f93331539a0ea84c0d898b9078bfc
Kushal Bose <kushalbose92@gmail.com>
2024-02-21 01:58:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F 
 import numpy as np
 import os
 from sklearn.metrics.pairwise import cosine_similarity
@@ -6,6 +7,7 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm 
+import seaborn as sns
 
 from torch_geometric.utils import to_dense_adj
 import networkx as nx
@@ -23,42 +25,60 @@ def visualize(h, dataname, i, color):
     plt.yticks([])
     plt.scatter(z[:, 0], z[:, 1], s=10, c=color, cmap="Set1")
     plt.savefig(os.getcwd() + "/visuals/" + dataname + '_embedding_' + str(i) + '.png')
+    plt.close()
 
 def mask_generation(index, num_nodes):
     mask = torch.zeros(num_nodes, dtype = torch.bool)
     mask[index] = 1
     return mask
 
-def label_guided_similarity(data, pred, alpha, beta, gamma):
-    pred = pred.cpu()
-    #Class Label Dictionary Creation
-    pred[np.where(data.train_mask)[0]] = data.y[np.where(data.train_mask)[0]].cpu()
-    dict_of_labels = {label : np.where(pred == label)[0] for label in range(data.num_classes)}
+def label_guided_similarity(data, pred, Y, X_c):
+    # pred = pred.cpu()
+    # #Class Label Dictionary Creation
+    # pred[np.where(data.train_mask)[0]] = data.y[np.where(data.train_mask)[0]].cpu()
+    # dict_of_labels = {label : np.where(pred == label)[0] for label in range(data.num_classes)}
+    
+    # dict_of_labels = class_dict_creation(data, pred)
 
     # Feature Similarity Matrix
     feature_similarity = cosine_similarity(data.x.cpu())
 
-    # Label Guided Similarity Matrix
-    label_score_matrix = np.full((data.num_nodes, data.num_nodes), gamma)
-    for i in range(data.num_nodes):
-        label_score_matrix[i][dict_of_labels[pred[i].item()]] = beta
-        if i in np.where(data.train_mask)[0]:
-            class_label = data.y[i].item()
-            same_class_train = np.intersect1d(np.where(data.y.cpu() == class_label)[0], np.where(data.train_mask)[0])
-            label_score_matrix[i][same_class_train] = alpha
-        label_score_matrix[i][i] = 1
+    # # Label Guided Similarity Matrix
+    # label_score_matrix = np.full((data.num_nodes, data.num_nodes), gamma)
+    # for i in range(data.num_nodes):
+    #     label_score_matrix[i][dict_of_labels[pred[i].item()]] = beta
+    #     if i in np.where(data.train_mask)[0]:
+    #         class_label = data.y[i].item()
+    #         same_class_train = np.intersect1d(np.where(data.y.cpu() == class_label)[0], np.where(data.train_mask)[0])
+    #         label_score_matrix[i][same_class_train] = alpha
+    #     label_score_matrix[i][i] = 1
 
-    # Hadamard product
-    label_score_matrix = np.zeros((data.num_nodes, data.num_nodes))
-    modified_similarity = np.multiply(feature_similarity, label_score_matrix)
+    # # Hadamard product
+    # label_score_matrix = np.zeros((data.num_nodes, data.num_nodes))
+    # modified_similarity = np.multiply(feature_similarity, label_score_matrix)
+    
+    label_score_matrix = F.softmax(torch.mm(torch.mm(Y, X_c), torch.t(torch.mm(Y, X_c))))
+    modified_similarity = np.multiply(feature_similarity, label_score_matrix.detach().cpu().numpy())
     # print(modified_similarity.shape)
-    return dict_of_labels, modified_similarity
+    
+    # return feature_similarity
+    return modified_similarity
 
 
-def manipulate_neighbours(data, pred, delete_k1, insert_k2, alpha, beta, gamma):
+def class_dict_creation(data, pred):
+    pred = pred.cpu()
+    #Class Label Dictionary Creation
+    pred[np.where(data.train_mask)[0]] = data.y[np.where(data.train_mask)[0]].cpu()
+    dict_of_labels = {label : np.where(pred == label)[0] for label in range(data.num_classes)}
+    
+    return dict_of_labels
+
+
+def manipulate_neighbours(data, pred, delete_k1, insert_k2, Y, X_c):
     delete_k = delete_k1
     insert_k = insert_k2
-    dict_of_labels , sim = label_guided_similarity(data, pred, alpha, beta, gamma)
+    sim = label_guided_similarity(data, pred, Y, X_c)
+    dict_of_labels = class_dict_creation(data, pred)
     edges = []
     pred = pred.cpu()
     data.edge_index = data.edge_index.cpu()
@@ -135,29 +155,48 @@ def manipulate_neighbours(data, pred, delete_k1, insert_k2, alpha, beta, gamma):
     return new_edge_index
 
 
-# def draw_graph(edge_index, train_idx, valid_idx, test_idx):
+def class_compatibility(y, edge_index, dataname):
+    device = edge_index.device
+    num_labels = max(y).item() + 1
+    num_nodes = y.shape[0] + 1
+    print(num_nodes, "   ", num_labels)
+    Y = torch.zeros(num_nodes, num_labels).to(device)
+    Y = Y.scatter_(1, y.unsqueeze(1), 1)
+    A = to_dense_adj(edge_index, max_num_nodes=num_nodes)[0]
+    # A += (torch.eye(num_nodes)* 1e-10).to(device)
+    E = torch.ones(num_nodes, num_labels).to(device)
+    num = torch.matmul(torch.matmul(torch.t(Y), A), Y)
+    den = torch.matmul(torch.matmul(torch.t(Y), A), E)
+    cc_matrix = num / (den + 1e-3)
+    cc_matrix = cc_matrix.detach().cpu().numpy()
 
-#     num_nodes = len(train_idx)
-#     adj_matrix = torch.zeros(num_nodes, num_nodes)
-#     for e in range(len(edge_index[0])):
-#         src = edge_index[0][e]
-#         tgt = edge_index[1][e]
-#         adj_matrix[src][tgt] = 1
-#     print("number of nodes ", num_nodes)
-#     color = ['green', 'blue', 'red']
-#     dict = {}
-#     for i in range(len(train_idx)):
-#         if train_idx[i] != 0:
-#             dict[i] = color[0]
-#     for j in range(len(valid_idx)):
-#         if valid_idx[j] != 0:
-#             dict[j] = color[1]
-#     for k in range(len(test_idx)):
-#         if test_idx[k] != 0:
-#             dict[k] = color[2]
-#     color_list = [dict[n] for n in range(num_nodes)]
-#     nx.draw(adj_matrix, node_color=color_list)
-#     plt.savefig(os.getcwd() + "/graph_view.png")
+    # Create a heatmap with more arguments
+    ax = sns.heatmap(
+            cc_matrix,
+            annot=True,          # Show numerical annotations in each cell
+            fmt='.4f',           # Format for annotations (two decimal places)
+            cmap='Oranges_r',     # Colormap (coolwarm is just an example, choose any other)
+            cbar=False,           # Show colorbar
+            linewidths=0.5,      # Width of the lines that divide each cell
+            square=True,         # Make the cells square-shaped
+            xticklabels=True,  # Custom x-axis labels
+            yticklabels=True,  # Custom y-axis labels
+            # cbar_kws={'yticklabels': []},  # Custom label for the colorbar
+    )
+    # cbar = ax.collections[0].colorbar
+    # cbar.set_ticks([])
+
+    # Add labels and title
+    # plt.xlabel('X-axis (Columns)')
+    # plt.ylabel('Y-axis (Rows)')
+    # plt.title('Customized Heatmap Example')
+
+    plt.savefig(os.getcwd() + "/class_compatibility_matrix_" + dataname + "_.png")
+    plt.close()
+
+    return cc_matrix
+
+
 
 
 
